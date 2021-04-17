db8de5bacbf1882a4ee7966ad355150dc7fdc765
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-17 17:20:12 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add
+{
+public static void main(String args[])
+{
+Add an=new Add();
+an.addition(5,8);
+}
+int result;
+void addition(int a,int b)
+{
+result=a+b;
+System.out.println("The Addition of two numbers is="+result);
+}
+}
