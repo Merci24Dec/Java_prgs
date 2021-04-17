cb0992ec4d99b30fcaacb56f08b188737a26b7f1
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-17 17:27:54 +0530

    Add files via upload

---
+++ exe.java
@@ -0,0 +1,16 @@
+class exe
+{
+	static int x;
+	public void read()
+	{
+		x=x+1;
+		System.out.println("object number="+x);
+	}
+	public static void main(string args[])
+	{
+		ob.read();
+		ob1.read();
+     
+	}
+}
+	
