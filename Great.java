a5915ccc31c788d3ee97ed42543af611c899948e
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-18 21:42:02 +0530

    Add files via upload

---
+++ Great.java
@@ -0,0 +1,25 @@
+class Great
+{
+public static void main(String args[])
+{
+int a,b,c;
+a=10;
+b=20;
+c=30;
+if((a>b)&&(a>c))
+{
+System.out.println("The Greatest is a"+a);
+}
+else if(b>c)
+{
+System.out.println("The Greatest is b"+c);
+}
+else 
+{
+System.out.println("The Greatest is c"+c);
+}
+}
+}
+
+
+
