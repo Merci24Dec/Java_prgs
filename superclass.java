f2b40fbeb06a39843583819fd5bb42abda03ac59
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-20 17:41:12 +0530

    Add files via upload

---
+++ superclass.java
@@ -0,0 +1,23 @@
+class one
+{
+ int i=15;
+ void show()
+{
+   System.out.println("super class");
+}
+}
+class two extend one
+int i=30;
+void show()
+{
+System.out.println("sub class");
+System.out.println(i);
+System.out.println(super i);
+super.show();
+}
+class demo
+{
+public static void main(String args[])
+two.b1=new two();
+b1.show()
+}
