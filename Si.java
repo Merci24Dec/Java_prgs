990ffa217b9bf4ef4739b558784f5665ebcd874b
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-20 17:39:20 +0530

    Add files via upload

---
+++ Si.java
@@ -0,0 +1,13 @@
+class Si
+{
+public static void main(String args[])
+{
+float si,r;
+int p,t;
+p=Integer.parseInt(args[0]);
+r=Float.parseFloat(args[1]);
+t=Integer.parseInt(args[2]);
+si=(p*r*t)/100;
+System.out.println("Simple Intrest for the given data is="+si);
+}
+}
