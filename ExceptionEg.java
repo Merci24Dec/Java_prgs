d6170764824222381c596fcf273556b71ea720cc
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-23 23:32:03 +0530

    Add files via upload

---
+++ ExceptionEg.java
@@ -0,0 +1,15 @@
+public class ExceptionEg
+{
+	public static void main(String[] args)
+	{
+	int a=5,b=0;
+		try
+		{
+		int c=a/b;
+		}
+		catch(ArithmeticException e)
+		{
+		e.printStackTrace();
+		}
+	}
+}
