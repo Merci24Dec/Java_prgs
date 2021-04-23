bff046e663cbc59f55daa39601d700a9ddd89ad1
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-23 23:28:40 +0530

    Add files via upload

---
+++ Unboxing.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class Unboxing
+{
+ public static void main(String args[])
+{
+char ch='a';
+Character a =ch;
+ArrayList<Integer>arrayList=new ArrayList<Integer>();
+arrayList.add(24);
+int num=arrayList.get(0);
+System.out.println(num);
+}
+}
