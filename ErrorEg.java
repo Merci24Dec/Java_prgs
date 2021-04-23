03711694133108b38e012881bc78474140ef2994
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-23 23:31:30 +0530

    Add files via upload

---
+++ ErrorEg.java
@@ -0,0 +1,19 @@
+class StackOverflow
+{
+	public static void test(int i)
+	{
+	if(i==0)
+	return;
+	else
+		{
+		test(i++);
+		}
+	}
+}
+public class ErrorEg
+{
+	public static void main(String[] args)
+	{
+	StackOverflow.test(5);
+	}
+}
