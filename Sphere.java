0ba3b6e2bc3dd0763645ab576b6b5afc1ceb949e
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-24 23:22:42 +0530

    Add files via upload

--- Sphere.java
+++ Sphere.java
@@ -1,14 +1,13 @@
-import java.io.*;
-class Sphere
+import java.util.Scanner;
+public class Sphere
 {
-public static void main(String args[])throws IOException
-{
-BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
-double r;
-System.out.println("Enter the radius of a sphere");
-r=Double.parseDouble(br.readLine());
-double vol=(4/3)*3.141*(r*r*r);
-System.out.println("Volume of the sphere is="+vol);
+ public static void main(String args[])
+ {
+  double r;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter radius of the sphere:-");
+   r=sc.nextInt();
+  double vol=(4/3)*3.141*(r*r*r);
+  System.out.println("Volume of the sphere is "+vol);
+ }
 }
-}
-
