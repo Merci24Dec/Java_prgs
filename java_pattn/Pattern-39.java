c29abd6c96d7a3e9f53f3bcea30944e0ed3a41e0
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-12 08:45:38 +0530

    Create Pattern-39.java

---
+++ java_pattn/Pattern-39.java
@@ -0,0 +1,32 @@
+public class Pattern39
+{
+	public static void main(String[] args)
+	{
+		int rows = 5;
+		System.out.println("## Printing the pattern ##");
+		for (int i = 1; i <= rows; i++) 
+                { 
+                        for (int j = rows; j > i; j--)
+			{
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++)
+			{
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+		for (int i = 1; i <= rows; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= rows - i; k++)
+			{
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+	}
+}
