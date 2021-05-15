7b69bbc67541bb04fdf55b51fe237196e41df64f
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-15 21:25:16 +0530

    Create PrintArrayListMain5.java

---
+++ java_tricky/Print_ArrayList_in_java_using_for_loop/PrintArrayListMain5.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+ 
+public class PrintArrayListMain5 {
+ 
+    public static void main(String args[])
+    {
+        ArrayList<String> fruitList = new ArrayList<>();
+ 
+        fruitList.add("Apple");
+        fruitList.add("Banana");
+        fruitList.add("Grapes");
+        fruitList.add("Orange");
+        System.out.println("Elements of ArrayList are:"+fruitList);
+    }
+}
