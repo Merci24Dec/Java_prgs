ba745f40936405c2dfb72cda7843c3d6964e1e81
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-15 20:53:12 +0530

    Create remove_whitespaces_without_replace.java

---
+++ java_tricky/remove_whitespaces_without_replace.java
@@ -0,0 +1,16 @@
+//18. Write a Java Program to remove all white spaces from a string with using replace().
+class RemoveWhiteSpaces
+{
+    public static void main(String[] args)
+    {
+        String str1 = "Saket Saurav        is a QualityAna    list";
+  
+        //1. Using replaceAll() Method
+  
+        String str2 = str1.replaceAll("\\s", "");
+  
+        System.out.println(str2);
+  
+           }
+}
+}
