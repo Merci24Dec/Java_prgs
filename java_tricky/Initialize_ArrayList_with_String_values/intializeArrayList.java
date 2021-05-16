4176a5bd511914afaa3c35386164e022ec7319ea
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-16 21:32:02 +0530

    Create intializeArrayList.java

---
+++ java_tricky/Initialize_ArrayList_with_String_values/intializeArrayList.java
@@ -0,0 +1,4 @@
+//intialize ArrayList with Integer values
+ArrayList<Integer> integerlist = new ArrayList<>(Arrays.asList(10,20,30,40,50)); 
+//intialize ArrayList with float values
+ArrayList<Integer> integerlist = new ArrayList<>(Arrays.asList(10.2f,20.4f,30.2f,40.9f,50.4f));
