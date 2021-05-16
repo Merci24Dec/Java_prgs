52ff93717baff93f1cc532ad81fc836e7692a51f
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-16 21:36:24 +0530

    Create Main4.java

---
+++ java_tricky/Initialize_ArrayList_with_String_values/Main4.java
@@ -0,0 +1,15 @@
+import java.util.List;
+public class Main4
+{   
+    public static void main(String args[]) 
+    {
+        List<String> list = List.of(
+                "Apple",
+                "Mango",
+                "Orange"
+                );
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+}
