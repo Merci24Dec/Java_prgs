80ec383639f516af881d111be2afe1a4f895973a
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-18 22:19:03 +0530

    Create Main1.java

---
+++ java_tricky/Magic_number/Main1.java
@@ -0,0 +1,11 @@
+public class Main1{
+ 
+    public static void main(String[] args) { 
+        int number = 10001;  // Number to check
+        if(number % 9 == 1) {
+            System.out.println("It is a magic number");
+        }else {
+            System.out.println("It is not a magic number");
+        }
+    }
+}
